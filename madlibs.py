5d0ff24777f09c8da30f4d76e30cfbfe4f7f02f3
Deon Steyn <32790719+Steyn81@users.noreply.github.com>
2022-05-27 16:28:12 +0200

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+# string concatenation (putting strings together)
+
+adj = input("Adjective: ")
+verb1 = input("Verb 1: ")
+verb2 = input("Verb 2: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
